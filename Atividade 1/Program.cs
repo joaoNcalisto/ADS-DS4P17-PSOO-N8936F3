c6bb9c044dc7ef765ef7b6649b9402918f0ea562
joaoNcalisto <joaocalisto13@gmail.com>
2023-09-17 14:17:52 -0300

    Envio das atividade 1 e 3 (Reenvio)

---
+++ Atividade 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public string RA { get; set; }
+
+    public Pessoa(string nome, string ra)
+    {
+        Nome = nome;
+        RA = ra;
+    }
+
+    public virtual string Apresentar()
+    {
+        return $"Meu nome é {Nome} e meu RA é {RA}.";
+    }
+}
+
+class Aluno : Pessoa
+{
+    public Aluno(string nome, string ra) : base(nome, ra)
+    {
+    }
+
+    public override string Apresentar()
+    {
+        return $"Sou o aluno {Nome} e meu RA é {RA}.";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno = new Aluno("Joao Vitor Nardini Calisto", "N8936F3");
+
+        Console.WriteLine(aluno.Apresentar());
+    }
+}
